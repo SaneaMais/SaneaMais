a56c77b25c6493fe0e93ccb8c9fdd20c6b6a7a8c
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2023-10-17 01:46:36 -0300

    teste

--- js/login/senha.js
+++ js/login/senha.js
@@ -10,8 +10,8 @@ inputIcon.forEach((item, i) => {
             'src',
             input[i].getAttribute('type') === 'password' ?
 
-                '../../img/Login/eye.svg'
-                : '../../img/Login/eye-off.svg'
+                '../img/Login/eye.svg'
+                : '../img/Login/eye-off.svg'
         );
 
         input[i].setAttribute(
