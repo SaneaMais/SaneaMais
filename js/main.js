0dd992756c90a0e71705f88d607901afe40a0e3c
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2023-10-19 01:22:09 -0300

    menu - pronto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const pageContent = document.querySelector(".page-content"); // Adicione uma classe "page-content" para o conteúdo que você deseja ocultar.
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
-
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  pageContent.classList.toggle("hidden"); // Toggle a classe "hidden" para ocultar ou mostrar o conteúdo da página.
+});
