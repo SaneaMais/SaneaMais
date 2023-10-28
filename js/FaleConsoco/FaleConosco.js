e4e8f07f695e5b30bb0750df86170a5fc94d8692
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2023-10-28 13:17:11 -0300

    colocanado img e nova página

--- js/FaleConsoco/FaleConosco.js
+++ js/FaleConsoco/FaleConosco.js
@@ -27,3 +27,14 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+/* ! menu */
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const pageContent = document.querySelector(".page-content"); // Adicione uma classe "page-content" para o conteúdo que você deseja ocultar.
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  pageContent.classList.toggle("hidden"); // Toggle a classe "hidden" para ocultar ou mostrar o conteúdo da página.
+});
