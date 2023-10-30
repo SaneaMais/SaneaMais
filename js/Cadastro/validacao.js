bce5791bbfd5e47cfcb1df3f7c926f72ab4b7d3d
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2023-10-30 01:42:48 -0300

    validação aprimorada login e desenvolvimento cadastro

--- js/Cadastro/validacao.js
+++ js/Cadastro/validacao.js
@@ -1,160 +1,197 @@
-/* let email = document.getElementById("email");
-let form = document.querySelector("form");
-let textForm = document.getElementById("textForm");
-let textEmail = document.getElementById("textEmail");
-
-let numero = document.querySelector('#numero')
-let labelNumero = document.querySelector('#labelNumero')
-let validNumero = false
-
-let nome = document.querySelector('#nome')
-let labelNome = document.querySelector('#labelNome')
-let validNome = false
-
-let data = document.querySelector('#data')
-let labelData = document.querySelector('#labelData')
-let validData = false
-
-let endereco = document.querySelector('#endereco')
-let labelEndereco = document.querySelector('#labelEndereco')
-let validEndereco = false
-
-let cep = document.querySelector('#cep')
-let labelCep = document.querySelector('#labelCep')
-let validCep = false
-
-let senha = document.getElementById("senha");
-let textPassword = document.getElementById("textPassword");
-
-let msgError = document.querySelector('#msgError')
-let msgSuccess = document.querySelector('#msgSuccess')
-
-
-email.addEventListener("keyup", () => {
-  if (validatorEmail(email.value) !== true) {
-    textEmail.style.color = 'red';
-    email.style.borderColor = 'red';
-    textEmail.textContent = "O formato do email deve ser Ex:name@abc.com";
-  } else {
-    textEmail.textContent = "";
-    email.style.borderColor = 'blue'; // Redefinir a cor da borda quando a validação passar.
-  }
-});
-
-
-function validatorEmail(email) {
-  let emailPattern =
-    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-  return emailPattern.test(email);
-}
-
-
-numero.addEventListener('keyup', () => {
-  if (numero.value.length < 15) {
-    textNumero.setAttribute('style', 'color: red', 'boder-color: red')
-    textNumero.textContent = "Insira até 11 números";
-  } else {
-    textNumero.textContent = "";
-  }
-})
-
-nome.addEventListener('keyup', () => {
-  if (nome.value.length <= 4) {
-    textNome.setAttribute('style', 'color: red', 'boder-color: red')
-    textNome.textContent = "Insira no minímo 5 caracteres";
-  } else {
-    textNome.textContent = "";
-  }
-})
-
-data.addEventListener('keyup', () => {
-  if (data.value.length <= 8) {
-    textData.setAttribute('style', 'color: red', 'boder-color: red')
-    textData.textContent = "Preencha corretamente o campo";
-  } else {
-    textData.textContent = "";
-  }
-})
-
-endereco.addEventListener('keyup', () => {
-  if (endereco.value.length <= 4) {
-    textEndereco.setAttribute('style', 'color: red', 'boder-color: red')
-    textEndereco.textContent = "Insira no minímo 3 caracteres, número e complemento";
-  } else {
-    textEndereco.textContent = "";
-  }
-})
-
-cep.addEventListener('keyup', () => {
-  if (cep.value.length < 9) {
-    textCep.setAttribute('style', 'color: red', 'boder-color: red')
-    textCep.textContent = "Insira corretamente 8 números";
-  } else {
-    textCep.textContent = "";
-  }
-})
-
-password.addEventListener("keyup", () => {
-  if (validatorPassword(password.value) !== true) {
-    textPassword.setAttribute('style', 'color: red', 'boder-color: red')
-    textPassword.textContent =
-      "O formato da senha deve ser no min 6 digitos";
-  } else {
-    textPassword.textContent = "";
-  }
-});
-
-function validatorPassword(password) {
-  let passwordPattern =
-    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-  return passwordPattern.test(password);
-}
-function cadastrar() {
-  form.addEventListener("submit", (e) => {
-    if (email.value == "" || password.value == "" || !validNumero || !validNome || !validData || !validEndereco || !validCep) {
-      textForm.textContent = "Você precisa preencher todos os campos!";
-      e.preventDefault(); // Impedir o envio do formulário
-    } else if (validatorEmail(email.value) === true && validatorPassword(password.value) === true) {
-      console.log(email.value);
-      textForm.textContent = "";
-      textEmail.textContent = "";
-      console.log(password.value);
-      textPassword.textContent = "";
-
-      setTimeout(() => {
-        window.location.href = 'file:///C:/Users/carol/OneDrive/Anexos/login/index.html';
-      }, 2000);
-    } else {
-      console.log("Requisição não atendida");
-      e.preventDefault(); // Impedir o envio do formulário
+/* Máscaras */
+function mascaraData(input) {
+  input.addEventListener("input", function () {
+    if (/\D/.test(input.value)) input.value = input.value.replace(/\D/g, "");
+    var v = input.value.replace(/\D/g, "").slice(0, 8);
+    if (v.length >= 2) {
+      v = v.replace(/(\d{2})(\d)/, "$1/$2");
     }
+    if (v.length >= 5) {
+      v = v.replace(/(\d{2})(\d{2})(\d{0,4})/, "$1/$2$3");
+    }
+    input.value = v;
   });
 }
 
+function mascaraCEP(input) {
+  input.addEventListener("input", function () {
+    if (/\D/.test(input.value)) input.value = input.value.replace(/\D/g, "");
+    var v = input.value.replace(/\D/g, "").slice(0, 8);
+    if (v.length >= 5) {
+      v = v.replace(/(\d{5})(\d{0,3})/, "$1-$2");
+    }
+    input.value = v;
+  });
+}
+function mascaraTelefone(input) {
+  input.addEventListener("input", function () {
+    var v = input.value.replace(/\D/g, "");
+    if (v.length <= 10) {
+      v = v.replace(/(\d{2})(\d{4})(\d{0,4})/, "($1) $2-$3");
+    } else {
+      v = v.replace(/(\d{2})(\d{5})(\d{0,4})/, "($1) $2-$3");
+    }
+    input.value = v.slice(0, 15);
+  });
+}
 
-/* function cadastrar(){
-form.addEventListener("submit", (e) => {
-  if (email.value == "" && password.value == "") {
-    textForm.textContent = "Você precisa preencher todos os campos!";
-  } else if (
-    validatorEmail(email.value) === true &&
-    validatorPassword(password.value) === true 
-  ) {
-    console.log(email.value);
-    textForm.textContent = "";
-    textEmail.textContent = "";
-    console.log(password.value);
-    textPassword.textContent = "";
+/* Máscaras */
+// Códigos das máscaras (não foram alterados)
+/* Máscaras */
+// Códigos das máscaras (não foram alterados)
 
+/* Validação */
+var password = document.getElementById("password");
+var messageSenha = document.getElementById("textPassword");
+var entrar = document.querySelector("button");
+var inputEmail = document.getElementById("email");
+var emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+var messageEmail = document.getElementById("textEmail");
+var numero = document.getElementById("numero");
+var messageNumero = document.getElementById("textNumero");
+var nome = document.getElementById("nome");
+var messageNome = document.getElementById("textNome");
+var data = document.getElementById("data");
+var messageData = document.getElementById("textData");
+var cep = document.getElementById("cep");
+var messageCep = document.getElementById("textCep");
 
-    setTimeout(()=>{
-      window.location.href = 'file:///C:/Users/carol/OneDrive/Anexos/login/index.html'
-  }, 2000)
+mascaraData(data);
+mascaraCEP(cep);
+mascaraTelefone(numero);
 
-  } else {
-    console.log("Requisição não atendida");
+function validateInput() {
+  var inputValue = inputEmail.value;
+  var numeroValue = numero.value;
+  var nomeValue = nome.value;
+  var dataValue = data.value;
+  var cepValue = cep.value;
+
+  var valid = true;
+
+  if (inputEmail.value === "") {
+    messageEmail.textContent = "Campo obrigatório";
+    messageEmail.style.color = "red";
+    inputEmail.style.border = "1px solid red";
+    valid = false;
+  } else if (emailRegex.test(inputValue)) {
+    messageEmail.textContent = "";
+    messageEmail.style.color = "";
+    inputEmail.style.border = "";
   }
 
-  e.preventDefault();
+  if (numero.value === "") {
+    messageNumero.textContent = "Campo obrigatório";
+    messageNumero.style.color = "red";
+    numero.style.border = "1px solid red";
+    valid = false;
+  } else if (/^\d{12}$/.test(numeroValue)) {
+    messageNumero.textContent = "";
+    messageNumero.style.color = "";
+    numero.style.border = "";
+  }
+
+  if (nome.value === "") {
+    messageNome.textContent = "Campo obrigatório";
+    messageNome.style.color = "red";
+    nome.style.border = "1px solid red";
+    valid = false;
+  } else if (nomeValue.trim().length >= 3) {
+    messageNome.textContent = "";
+    messageNome.style.color = "";
+    nome.style.border = "";
+  }
+
+  if (data.value === "") {
+    messageData.textContent = "Campo obrigatório";
+    messageData.style.color = "red";
+    data.style.border = "1px solid red";
+    valid = false;
+  } else if (/^\d{8}$/.test(dataValue)) {
+    messageData.textContent = "";
+    messageData.style.color = "";
+    data.style.border = "";
+  }
+
+  if (cep.value === "") {
+    messageCep.textContent = "Campo obrigatório";
+    messageCep.style.color = "red";
+    cep.style.border = "1px solid red";
+    valid = false;
+  } else if (/^\d{8}$/.test(cepValue)) {
+    messageCep.textContent = "";
+    messageCep.style.color = "";
+    cep.style.border = "";
+  }
+
+  return valid;
+}
+
+function senhaValida() {
+  if (password.value.length >= 8) {
+    messageSenha.textContent = "";
+    messageSenha.style.color = "";
+    password.style.border = "";
+    return true;
+  } else {
+    messageSenha.textContent = "Senha inválida (mínimo 8 caracteres)";
+    messageSenha.style.color = "red";
+    password.style.border = "1px solid red";
+    return false;
+  }
+}
+
+entrar.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var inputIsValid = validateInput();
+  var senhaIsValid = senhaValida();
+
+  if (inputIsValid && senhaIsValid) {
+    document.getElementById("msgError").textContent = "";
+    alert("Formulário válido. Você pode prosseguir.");
+  } else {
+    document.getElementById("msgError").textContent = "Por favor, corrija os erros no formulário.";
+  }
 });
-} */ 
+
+// Adicionar event listeners de input para campos relevantes
+inputEmail.addEventListener("input", function () {
+  if (emailRegex.test(inputEmail.value)) {
+    messageEmail.textContent = "";
+    messageEmail.style.color = "";
+    inputEmail.style.border = "";
+  }
+});
+
+numero.addEventListener("input", function () {
+  if (/^\d{12}$/.test(numero.value)) {
+    messageNumero.textContent = "";
+    messageNumero.style.color = "";
+    numero.style.border = "";
+  }
+});
+
+nome.addEventListener("input", function () {
+  if (nome.value.trim().length >= 3) {
+    messageNome.textContent = "";
+    messageNome.style.color = "";
+    nome.style.border = "";
+  }
+});
+
+data.addEventListener("input", function () {
+  if (/^\d{8}$/.test(data.value)) {
+    messageData.textContent = "";
+    messageData.style.color = "";
+    data.style.border = "";
+  }
+});
+
+cep.addEventListener("input", function () {
+  if (/^\d{8}$/.test(cep.value)) {
+    messageCep.textContent = "";
+    messageCep.style.color = "";
+    cep.style.border = "";
+  }
+});
