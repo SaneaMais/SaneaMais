182b076e7ad50e3cf9343eff9e4766ed16b480cd
Alex Farias Vaz dos Santos <alexfarias08032006@gmail.com>
2023-10-31 00:39:30 -0300

    testando

--- js/Cadastro/validacao.js
+++ js/Cadastro/validacao.js
@@ -1,4 +1,4 @@
-
+/* 
 var dataInput = document.getElementById("data");
 aplicarMascara(dataInput, "data");
 
@@ -126,3 +126,128 @@ function senhaValida(password) {
     return "valido";
   }
 }
+ */
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      validateForm();
+  });
+
+  const emailInput = document.getElementById("email");
+  const numeroInput = document.getElementById("numero");
+  const nomeInput = document.getElementById("nome");
+  const dataInput = document.getElementById("data");
+  const cepInput = document.getElementById("cep");
+  const passwordInput = document.getElementById("password");
+
+  emailInput.addEventListener("blur", function () {
+      validateEmail(emailInput);
+  });
+
+  numeroInput.addEventListener("blur", function () {
+      validatePhoneNumber(numeroInput);
+  });
+
+  nomeInput.addEventListener("blur", function () {
+      validateUsername(nomeInput);
+  });
+
+  dataInput.addEventListener("blur", function () {
+      validateDate(dataInput);
+  });
+
+  cepInput.addEventListener("blur", function () {
+      validateCep(cepInput);
+  });
+
+  passwordInput.addEventListener("blur", function () {
+      validatePassword(passwordInput);
+  });
+
+  function validateEmail(input) {
+      const email = input.value;
+      if (email.includes("@")) {
+          clearValidation(input);
+      } else {
+          setValidation(input, "Email inválido");
+      }
+  }
+
+  function validatePhoneNumber(input) {
+      const phoneNumber = input.value.replace(/\D/g, "");
+      if (phoneNumber.length === 12) {
+          clearValidation(input);
+      } else {
+          setValidation(input, "Número de telefone inválido");
+      }
+  }
+
+  function validateUsername(input) {
+      const username = input.value;
+      if (username.length >= 3) {
+          clearValidation(input);
+      } else {
+          setValidation(input, "Nome inválido");
+      }
+  }
+
+  function validateDate(input) {
+      const date = input.value.replace(/\D/g, "");
+      if (date.length === 8) {
+          clearValidation(input);
+      } else {
+          setValidation(input, "Data de nascimento inválida");
+      }
+  }
+
+  function validateCep(input) {
+      const cep = input.value.replace(/\D/g, "");
+      if (cep.length === 8) {
+          clearValidation(input);
+      } else {
+          setValidation(input, "CEP inválido");
+      }
+  }
+
+  function validatePassword(input) {
+      const password = input.value;
+      if (password.length >= 8) {
+          clearValidation(input);
+      } else {
+          setValidation(input, "Senha inválida");
+      }
+  }
+
+  function clearValidation(input) {
+      input.style.border = "";
+      const errorMessage = input.nextElementSibling;
+      errorMessage.textContent = "";
+  }
+
+  function setValidation(input, message) {
+      input.style.border = "1px solid red";
+      const errorMessage = input.nextElementSibling;
+      errorMessage.textContent = message;
+      errorMessage.style.color = "red";
+  }
+
+  function validateForm() {
+      let isValid = true;
+      const inputs = [emailInput, numeroInput, nomeInput, dataInput, cepInput, passwordInput];
+
+      inputs.forEach(function (input) {
+          if (input.value.trim() === "") {
+              setValidation(input, "Campo obrigatório");
+              isValid = false;
+          }
+      });
+
+      if (isValid) {
+          form.submit(); // Envie o formulário se for válido
+      }
+  }
+});
--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
   pageContent.classList.toggle("hidden"); 
 });
+
+document.querySelector('footer a[href="#meu-card"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  
+  const cardElement = document.getElementById('meu-card');
+  
+  if (cardElement) {
+    cardElement.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
